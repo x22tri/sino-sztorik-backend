7ca4faaac35039d3bdf70d77a4c22664d14575fd
x22tri <x22tri@gmail.com>
2022-03-26 19:21:01 +0100

    added server

--- util/database.js
+++ util/database.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize')
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {dialect: 'mysql', logging: false})
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST, dialect: 'mysql', logging: false
+})
 console.log('name user pw' + process.env.DB_NAME + process.env.DB_USER + process.env.DB_PASSWORD)
 console.log('port' + process.env.PORT)
 console.log('frontend' + process.env.FRONTEND_URL)
