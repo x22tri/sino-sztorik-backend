39a5cc9219ddab208a0cbb5047a7dc8d16412eda
x22tri <x22tri@gmail.com>
2022-03-26 18:57:18 +0100

    dotenv

--- util/database.js
+++ util/database.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize')
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {dialect: 'mysql', logging: false})
+console.log(process.env.DB_USER)
 
 module.exports = sequelize
