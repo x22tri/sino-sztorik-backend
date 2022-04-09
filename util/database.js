9cb3512785fe9cead0d644b60990e39e59a44f0d
x22tri <x22tri@gmail.com>
2022-04-09 13:16:55 +0200

    Add connection pooling

--- util/database.js
+++ util/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || undefined, dialect: 'mysql', logging: false
+    host: process.env.DB_HOST || undefined, dialect: 'mysql', logging: false,
+    pool: { max: 3 }
 })
 
 module.exports = sequelize
