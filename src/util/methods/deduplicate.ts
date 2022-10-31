94677daf334d43eca89face30ea024e5fe9a8236
x22tri <x22tri@gmail.com>
2022-10-31 13:04:21 +0100

    Upgrade TS to 4.9 beta to use satisfies

--- src/util/methods/deduplicate.ts
+++ src/util/methods/deduplicate.ts
@@ -11,10 +11,10 @@ function isPropertyKey(value: Model[keyof Model]): value is PropertyKey {
 /**
  * In an array of Sequelize model instances, filters out duplicates based on the given field.
  * In other words, for objects where the given field's value is the same, it will only keep the first one found.
- * @param {Object.<Model[], PropertyKey>} parameters
- * @param {Model[]} parameters.array - The array of objects to filter.
- * @param {PropertyKey} parameters.byField - The name of the property based on which to filter.
- * @returns {void} Modifies `array`.
+ *
+ * @param parameters.array - The array of objects to filter.
+ * @param parameters.byField - The name of the property based on which to filter.
+ * @returns Modifies `array`.
  */
 function deduplicate<M extends Model>({
   array,
@@ -28,6 +28,12 @@ function deduplicate<M extends Model>({
   for (let i = 0; i < array.length; i++) {
     const currentValue = array[i].getDataValue(byField);
 
+    if (!currentValue) {
+      throw new Error(
+        `The field ${String(byField)} does not exist on the object.`
+      );
+    }
+
     if (!isPropertyKey(currentValue)) {
       throw new Error(`This field can't be used to remove duplicates by.`);
     }
@@ -38,8 +44,6 @@ function deduplicate<M extends Model>({
       lookup[currentValue] = true;
     }
   }
-
-  lookup = {};
 
   return;
 }
