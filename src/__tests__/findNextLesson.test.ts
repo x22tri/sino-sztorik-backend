03cd243fd769a0737a45cc836aee4bda229b4e8e
x22tri <x22tri@gmail.com>
2022-11-13 12:04:15 +0100

    Refactor unit test for findNextLesson

--- src/__tests__/findNextLesson.test.ts
+++ src/__tests__/findNextLesson.test.ts
@@ -7,47 +7,51 @@ import moquelize from '../util/moquelize.js';
 import CharacterOrder from '../models/character-orders.js';
 import { Progress } from '../util/interfaces.js';
 
-let findNextLesson: (
-  currentTier: number,
-  currentLesson: number
-) => Promise<Progress>;
-
-async function mockCharacterOrderDatabase(testData: [number, number][]) {
-  const databaseInstances = testData.map(([tier, lessonNumber]) =>
-    mockCharacterOrderInstance(tier, lessonNumber)
-  );
-
-  jest.unstable_mockModule('../models/character-orders.ts', () => ({
-    __esModule: true,
-    default: moquelize(databaseInstances),
-  }));
-
-  const imported = await import('../controllers/users/utils/findNextLesson.js');
-
-  findNextLesson = imported.default;
-}
-
-function mockCharacterOrderInstance(tier: number, lessonNumber: number) {
-  return CharacterOrder.build({
-    tier,
-    lessonNumber,
-    // The following properties are obligatory for a CharacterOrder instance but are not tested by findNextLesson.
-    indexInLesson: 1,
-    orderId: 99,
-    charId: 'test',
-  });
-}
-
-function mockProgressInstance(tier: number, lessonNumber: number): Progress {
-  return { tier, lessonNumber };
-}
-
 describe('findNextLesson()', function () {
+  let findNextLesson: (
+    currentTier: number,
+    currentLesson: number
+  ) => Promise<Progress>;
+
+  async function mockCharacterOrderDatabase(testData: [number, number][]) {
+    const databaseInstances = testData.map(([tier, lessonNumber]) =>
+      mockCharacterOrderInstance(tier, lessonNumber)
+    );
+
+    jest.unstable_mockModule('../models/character-orders.ts', () => ({
+      __esModule: true,
+      default: moquelize(databaseInstances),
+    }));
+
+    const imported = await import(
+      '../controllers/users/utils/findNextLesson.js'
+    );
+
+    findNextLesson = imported.default;
+  }
+
+  function mockCharacterOrderInstance(tier: number, lessonNumber: number) {
+    return CharacterOrder.build({
+      tier,
+      lessonNumber,
+      // The following properties are obligatory for a CharacterOrder instance but are not tested by findNextLesson.
+      indexInLesson: 1,
+      orderId: 99,
+      charId: 'test',
+    });
+  }
+
+  function mockProgressInstance(tier: number, lessonNumber: number): Progress {
+    return { tier, lessonNumber };
+  }
+
   afterEach(() => {
     jest.resetModules();
   });
 
-  it('finds the next lesson in the same tier when exists, even when database entries are in wrong order', async () => {
+  // Tests start here.
+
+  it('finds the next lesson in the same tier if it exists, even if database entries are in wrong order', async () => {
     await mockCharacterOrderDatabase([
       [1, 1],
       [1, 3],
@@ -61,7 +65,7 @@ describe('findNextLesson()', function () {
     expect(nextLesson).toEqual(mockProgressInstance(1, 2));
   });
 
-  it(`finds the next lesson in the same tier when exists, even when lesson number is not the number directly after user's lesson`, async () => {
+  it(`finds the next lesson in the same tier if it exists, even if lesson number is not the number directly after user's lesson`, async () => {
     await mockCharacterOrderDatabase([
       [1, 1],
       [1, 3],
@@ -74,7 +78,7 @@ describe('findNextLesson()', function () {
     expect(nextLesson).toEqual(mockProgressInstance(1, 3));
   });
 
-  it(`finds the first lesson in the next tier when no more lessons are found in the current tier`, async () => {
+  it(`finds the first lesson in the next tier if no more lessons are found in the current tier`, async () => {
     await mockCharacterOrderDatabase([
       [1, 1],
       [3, 2],
@@ -88,7 +92,7 @@ describe('findNextLesson()', function () {
     expect(nextLesson).toEqual(mockProgressInstance(2, 3));
   });
 
-  it(`returns the "course finished" progress state when user is at last tier and there are no more lessons left`, async () => {
+  it(`returns the "course finished" progress state if user is at last tier and there are no more lessons left`, async () => {
     await mockCharacterOrderDatabase([
       [1, 1],
       [3, 2],
@@ -100,7 +104,7 @@ describe('findNextLesson()', function () {
     expect(nextLesson).toMatchObject(COURSE_FINISHED);
   });
 
-  it(`returns error when user is not at last tier but no more lessons are found`, async () => {
+  it(`returns error if user is not at last tier but no more lessons are found`, async () => {
     await mockCharacterOrderDatabase([[1, 1]]);
 
     await expect(findNextLesson(LAST_TIER - 1, 10)).rejects.toThrow(
@@ -108,7 +112,7 @@ describe('findNextLesson()', function () {
     );
   });
 
-  it(`returns error when no valid lesson objects are found`, async () => {
+  it(`returns error if no valid lesson objects are found`, async () => {
     await mockCharacterOrderDatabase([]);
 
     await expect(findNextLesson(1, 1)).rejects.toThrow(
